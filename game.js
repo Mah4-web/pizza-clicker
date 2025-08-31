d0f008678c9754b9b77d58bed06def963a87ddfb
Mahpara <mah4.para@gmail.com>
2025-08-31 19:10:05 +0100

    feat: completed body elements, enhanced styling for additional components, updated fetch to retrieve options from an API

--- game.js
+++ game.js
@@ -15,3 +15,29 @@ let state = {
     pizzaCount: 0,
     cps: 0,
 };
+
+// Fetch upgrade options from an API
+
+async function displayUpgrades() {
+    const upgrades = await getUpgrades();
+    const upgradeContainer = document.getElementById("upgrades");
+
+    for (let i = 0; i < upgrades.length; i++) {
+    const btn = document.createElement("button");
+    btn.textContent = `${upgrades[i].name} - Cost: ${upgrades[i].cost} pizzas`;
+    btn.className = "upgradeButtons";
+    btn.setAttribute("index", i);
+    upgradeContainer.appendChild(btn);
+
+    btn.addEventListener("click", function () {
+    const index = this.getAttribute("index");
+    if (state.pizzaCount >= upgrades[index].cost) {
+        state.pizzaCount -= upgrades[index].cost;
+        state.cps += upgrades[index].increase;
+        updatePizzaAmount();
+    } else {
+        alert("You don't have enough pizzas!");
+    }
+    });
+}
+}
