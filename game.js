2a1af1e5e4d67b88305b8d2247a1e74dfa9ae216
Mahpara <mah4.para@gmail.com>
2025-08-31 18:11:10 +0100

    feat: created and styled header, implemented DOM elements, and added pizza count and CPS

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 console.log("Hello World, I Am Building My First Clicking (IDLE) Game!")
+
+// DOM elements and total pizza count and CPS 
+
+let pizzaAmount = document.getElementById("pizzaAmount");
+let cpsAmount = document.getElementById("cps");
+let toggleSoundBtn = document.getElementById("toggle-sound");
+let clickContainer = document.getElementById("click-animation-container");
+
+let pizzaSound = new Audio("Click.mp3");
+    pizzaSound.volume = 1;
+let soundOn = true;
+
+let state = {
+    pizzaCount: 0,
+    cps: 0,
+};
